b21a432a1fd193401b83f6b64e38288385a73abf
swc9803 <swc9803@gmail.com>
2021-06-30 17:09:03 +0900

    0630

--- src/pages/Info.vue
+++ src/pages/Info.vue
@@ -1,29 +1,28 @@
 <template>
   <div>
-    <h6>테스트용으로 만들어 실존하지 않는 가게입니다.</h6>
+    <h6>테스트용으로 만들었으며 실존하지 않는 가게입니다.</h6>
     <div id="map"></div>
   </div>
 </template>
 
 <script>
 export default {
-  mounted () {
-    window.kakao && window.kakao.maps
-      ? this.initMap()
-      : this.addKakaoMapScript()
-  },
-  setup () {
-    var container = document.getElementById('map')
-    var options = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667),
-      level: 3
-    }
-
-    var map = new kakao.maps.Map(container, options)
-    return {
-      map
-    }
-  }
+  // mounted () {
+  //   window.kakao && window.kakao.maps
+  //     ? this.initMap()
+  //     : this.addKakaoMapScript()
+  // },
+  // setup () {
+  //   var container = document.getElementById('map')
+  //   var options = {
+  //     center: new kakao.maps.LatLng(33.450701, 126.570667),
+  //     level: 3
+  //   }
+  //   var map = new kakao.maps.Map(container, options)
+  //   return {
+  //     map
+  //   }
+  // }
 }
 </script>
 
