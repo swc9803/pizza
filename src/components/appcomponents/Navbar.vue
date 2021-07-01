8c5532748d3b779b9604742756716e790dc3f9b7
swc9803 <swc9803@gmail.com>
2021-07-01 14:05:51 +0900

    0701

--- src/components/appcomponents/Navbar.vue
+++ src/components/appcomponents/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-    <div class="container-fluid">
+    <div class="container">
       <Symbol />
       <SymbolText />
       <div>
