dbad3ff2ea4e2390b382b5663b68facb59f9a8d0
swc9803 <swc9803@gmail.com>
2021-07-08 14:53:23 +0900

    0708

--- src/pages/Notice.vue
+++ src/pages/Notice.vue
@@ -2,24 +2,43 @@
   <div class="margin">
     신메뉴
     휴일
-    글쓰기 누르면  ifuser=글쓰기로 else 모달창,로그인후 글쓰기로
-    모달 안되면 페이지로
-    <button class="btn btn-primary" @click="moveToCreate">
-      글쓰기
-    </button>
-    <button class="btn btn-dark btn-block" @click="openModal">
-      로그인
-    </button>
-    <LoginModal
-      v-if="showModal"
-      @login="Login"
-      @close="closeModal"
-    />
+    <div v-if="user == ''">
+      <button class="btn btn-primary" @click="openModal">
+        글쓰기
+      </button>
+    </div>
+    <div v-else>
+      <button class="btn btn-primary" @click="moveToCreate">
+        글쓰기
+      </button>
+    </div>
+    <div v-if="showModal">
+      <div class="modal-wrapper" style="color: cyan;">
+        <div class="modal-dialog">
+          <div class="modal-content" style="background: rgb(50, 50, 50)">
+            <div class="modal-header m-2">
+              <h6 class="modal-title"><i class="fas fa-sign-out-alt"></i> 관리자만 글을 작성할 수 있습니다.</h6>
+            </div>
+            <div class="form form-group modal-body">
+              <label>Email</label>
+              <input type="email" v-model="email" class="form-control">
+            </div>
+            <div class="form form-group modal-body">
+              <label>Password</label>
+              <input type="password" v-model="password" class="form-control">
+            </div>
+            <div class="modal-footer m-2">
+              <button type="button" class="btn btn-primary" @click="Login">로그인</button>
+              <button type="button" class="btn btn-secondary" @click="closeModal">취소</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import LoginModal from '@/components/LoginModal'
 import { useRouter } from 'vue-router'
 import { ref } from 'vue'
 import { auth } from '../../fdb'
@@ -28,19 +47,15 @@ export default {
   setup () {
     const router = useRouter()
     const showModal = ref(false)
-    const openModal = () => {
-      showModal.value = true
-    }
-    const moveToCreate = () => {
-      router.push({
-        name: 'Create'
-      })
-    }
     const closeModal = () => {
       showModal.value = false
     }
     const email = ref('')
     const password = ref('')
+
+    const openModal = () => {
+      showModal.value = true
+    }
     const Login = async () => {
       try {
         await auth.signInWithEmailAndPassword(email.value, password.value)
@@ -50,18 +65,41 @@ export default {
         alert('에러 : ' + err.message)
       }
     }
+    const moveToCreate = () => {
+      router.push({
+        name: 'Create'
+      })
+    }
+    const LogOut = async () => {
+      await auth.signOut()
+      closeModal()
+      alert('로그아웃 되었습니다.')
+      router.push({ name: 'Home' })
+    }
     return {
       showModal,
       openModal,
       closeModal,
-      moveToCreate,
       email,
       password,
-      Login
+      Login,
+      moveToCreate,
+      LogOut
     }
   },
-  components: {
-    LoginModal
+  data () {
+    return {
+      user: ''
+    }
+  },
+  mounted () {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.user = user
+      } else {
+        this.user = ''
+      }
+    })
   }
 }
 </script>
@@ -70,5 +108,17 @@ export default {
 .margin {
   width: 80%;
   margin: auto;
+}
+.modal-wrapper {
+  position: fixed;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+}
+.form {
+  width: 70%;
 }
 </style>
